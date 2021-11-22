afb6f8f5009d99598e82b75cb568049d0edd07a4
jngnandan <jngnandan@gmail.com>
2021-11-22 20:34:07 +0000

    second commit

--- src/components/ShowHideApp/index.js
+++ src/components/ShowHideApp/index.js
@@ -6,52 +6,50 @@ class ShowHideApp extends Component {
         secondState: "false"
     }
 
-    Sample = () => {
-        const { presentState } = this.state
-        if (presentState === "true") {
-            return (
-                <div className="col-12 rounded bg-light m-auto m-1">
-                    <h1>Sherlock</h1>
-                </div>)
-        }
-    }
-
-    SecondSample = () => {
-        const { secondState } = this.state
-        if (secondState === "true") {
-            return (
-                <div className="col-12 rounded bg-light m-auto m-1">
-                    <h1>Holmes</h1>
-                </div>)
-        }
-    }
-
     render(props) {
         const { presentState, secondState } = this.state
         const { Sample } = this.props
+        let First
+        let Second
+
 
         const clickFirst = () => {
-            presentState === "true" ?
-                this.setState({
-                    presentState: "false"
-                }) :
+            presentState === "false" ?
                 this.setState({
                     presentState: "true"
+                }) :
+                this.setState({
+                    presentState: "false"
                 })
         }
 
         const clickSecond = () => {
-            secondState === "true" ?
-                this.setState({
-                    secondState: "false"
-                }) :
+            secondState === "false" ?
                 this.setState({
                     secondState: "true"
+                }) :
+                this.setState({
+                    secondState: "false"
                 })
         }
 
 
+        if (presentState === "true") {
+            First = <div className="col-12 rounded m-1 p-3 bg-light"><h1>Sherlock</h1></div>;
 
+        }
+        else if (presentState === "false") {
+            First = ""
+        }
+
+        if (secondState === "true") {
+            Second = <div className="col-12 rounded m-1 p-3 bg-light"><h1>Holmes</h1></div>;
+
+
+        }
+        else if (secondState === "false") {
+            Second = ""
+        }
 
         return (
             <div className="row vh-100">
@@ -60,19 +58,14 @@ class ShowHideApp extends Component {
                     <div className="d-flex flex-row justify-content-center">
                         <div className="col-5">
                             <button onClick={clickFirst} className="col-12 m-1 btn btn-light mb-3">First Name</button>
-                            {this.Sample()}
+                            {First}
                         </div>
 
                         <div className="col-5">
                             <button onClick={clickSecond} className="col-12 m-1 btn btn-light mb-3">Last Name</button>
-                            {this.SecondSample()}
+                            {Second}
                         </div>
                     </div>
-
-
-
-
-
 
                 </div>
             </div>
